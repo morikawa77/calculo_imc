2444a7f161dab2514023ec37d633e990084f5633
Reginaldo Morikawa <morikawa77@gmail.com>
2023-03-03 03:26:35 -0300

    ADDED widget switch for multiplatform app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+// web friendly check for apple based platforms
+bool isCupertino() => !kIsWeb && (Platform.isIOS || Platform.isMacOS);
+
+String helloText = "Hello World";
 
 void main() {
   runApp(App());
@@ -7,7 +16,17 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    if (isCupertino()) {
+      return CupertinoApp(
+        debugShowCheckedModeBanner: false,
+        title: helloText,
+        theme: CupertinoThemeData(
+          primaryColor: Colors.blueGrey,
+        ),
+      );
+    }
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // theme: ThemeData.dark(),
       theme: ThemeData(
         primarySwatch: Colors.green,
@@ -16,7 +35,7 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Icon(Icons.share),
         ),
-        body: Text('Hello World'),
+        body: Text(helloText),
       ),
     );
   }
