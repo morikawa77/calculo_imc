4e5c027cee88c0db59c5230c43d0474d46cdcb5d
Reginaldo Morikawa <morikawa77@gmail.com>
2023-03-08 07:54:28 -0300

    testing codes

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter/foundation.dart';
 // web friendly check for apple based platforms
 bool isCupertino() => !kIsWeb && (Platform.isIOS || Platform.isMacOS);
 
-String helloText = "Hello World - teste";
+String helloText = "Hello World";
 
 void main() {
   runApp(App());
