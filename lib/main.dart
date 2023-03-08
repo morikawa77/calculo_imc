d110cfb382bcdd18d05419b69f8787ef4bdbdd1a
Reginaldo Morikawa <morikawa77@gmail.com>
2023-03-08 08:52:51 -0300

    class 03082023

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,42 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
-
-// web friendly check for apple based platforms
-bool isCupertino() => !kIsWeb && (Platform.isIOS || Platform.isMacOS);
-
-String helloText = "Hello World";
 
 void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    if (isCupertino()) {
-      return CupertinoApp(
-        debugShowCheckedModeBanner: false,
-        title: helloText,
-        theme: CupertinoThemeData(
-          primaryColor: Colors.blueGrey,
-        ),
-      );
-    }
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // theme: ThemeData.dark(),
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.yellow,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: Icon(Icons.share),
         ),
-        body: Text(helloText),
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(
+              color: Colors.blue,
+              width: 200,
+            ),
+            Container(
+              color: Colors.red,
+              width: 200,
+            ),
+            Container(
+              color: Colors.green,
+              width: 200,
+            ),
+            Container(
+              color: Colors.yellow,
+              width: 200,
+            ),
+          ],
+        ),
       ),
     );
   }
