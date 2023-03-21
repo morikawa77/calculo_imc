d5101abca4abcbe73f1b1357f9801e40494a2544
Reginaldo Morikawa <morikawa77@gmail.com>
2023-03-20 22:40:04 -0300

    class 03152023

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,62 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  
+  final txtPeso = TextEditingController();
+  final txtAltura = TextEditingController();
+  final txtIMC = TextEditingController();
+
+  String imagem = 'images/abaixo.jpg';
+
+  double? imc;
+
+  void calcular() {
+    double peso = double.parse(txtPeso.text);
+    double altura = double.parse(txtAltura.text);
+
+    setState(() => imc = peso / (altura * altura));
+
+    if (imc! < 18.5)
+      setState(() {
+        imagem = 'images/abaixo.jpg';
+        txtIMC.text = imc.toString();
+      });
+    else if (imc! < 24.9)
+      setState(() {
+        imagem = 'images/ideal.jpg';
+        txtIMC.text = imc.toString();
+      });
+    else if (imc! < 29.9)
+      setState(() {
+        imagem = 'images/excesso.jpg';
+        txtIMC.text = imc.toString();
+      });
+    else if (imc! < 39.9)
+      setState(() {
+        imagem = 'images/obesidade1.jpg';
+        txtIMC.text = imc.toString();
+      });
+    else
+      setState(() {
+        imagem = 'images/obesidade2.jpg';
+        txtIMC.text = imc.toString();
+      });
+ 
+  }
+
+  void limpar() {
+    setState(() {
+      txtPeso.text = "";
+      txtAltura.text = "";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,31 +81,54 @@ class App extends StatelessWidget {
                 child: Column(
                   children: [
                     TextField(
+                      controller: txtPeso,
                       decoration: InputDecoration(
                         labelText: "Peso"
                       ),
                     ),
                     TextField(
+                      controller: txtAltura,
                       decoration: InputDecoration(
                         labelText: "Altura"
                       ),
                     ),
-                    ElevatedButton(
-                      onPressed: (){},
-                      child: Text("Calcular"),
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.all(10),
+                      child: ListTile(
+                        title: Row(
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: calcular,
+                                child: Text("Calcular"),
+                              ),
+                            ),
+                          Expanded(
+                            child: ElevatedButton(
+                            onPressed: limpar,
+                            child: Text("Limpar"),
+                            ),
+                          ),
+                          ],)
+                      )
                     ),
                   ],
                 ),
               ),
             ),
-            Flexible(
+            Expanded(
               flex: 2,
-              child: Container(
-                color: Colors.red,
-                margin: EdgeInsets.all(10),
-                child: Image.asset('images/ideal.jpg'),
+              child: FittedBox(
+                child: Image.asset(imagem),
               ),
             ),
+            TextField(
+                        controller: txtIMC,
+                        decoration: InputDecoration(
+                          labelText: "Valor do IMC"
+                        ),
+                      )
           ],
         ),
       ),
